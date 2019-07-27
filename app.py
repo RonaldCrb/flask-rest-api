9efd2ddf4a67012120bc6a5a1db2e37f3f92f09e
Ron Alonzo <alonzo.ronald@gmail.com>
2019-07-27 18:38:59 +0200

    falta crear base de datos sqlite y resolver el problema del pylint para visual studio

--- app.py
+++ app.py
@@ -1,26 +1,45 @@
-from flask import Flask, request, jsonify
+import os
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-import os
 
-
-app = Flask(__name__) # <== instanciamos la app
-
-basedir = os.path.abspath(os.path.dirname(__file__)) # <== creamos la variable de la ruta hacia el archivo sqlite
+# instanciamos la app
+app = Flask(__name__) 
+# creamos la variable de la ruta hacia el archivo sqlite
+basedir = os.path.abspath(os.path.dirname(__file__)) 
 # configuramos la base de datos sqlite
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Inicializamos la base de datos
+db = SQLAlchemy(app) 
+# Inicializamos Marshmallow
+ma = Marshmallow(app) 
 
-db = SQLAlchemy(app) # <== Inicializamos la base de datos
+# modelo del producto (que describe como debe ser el objeto)
+class Smartlog(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  date = db.column(db.DateTime)
+  name = db.column(db.String(50))
+  depth = db.column(db.Integer)
+  leave_surface = db.column(db.DateTime)
+  leave_bottom = db.column(db.DateTime)
+  bottom_time = db.column(db.Integer)
 
-ma = Marshmallow(app) # <== Inicializamos Marshmallow
+  def __init__(self, date, name, depth, leave_surface, leave_bottom, bottom_time):
+    self.date = date
+    self.name = name
+    self.depth = depth
+    self.leave_surface = leave_surface
+    self.leave_bottom = leave_bottom
+    self.bottom_time = bottom_time
+# se crea el schema (esquema) de el Smartlog
+class SmartlogSchema(ma.Schema):
+  class Meta:
+    fields = ('id', 'date', 'name', 'depth', 'leave_surface', 'leave_bottom', 'bottom_time')
 
-@app.route('/', methods=['GET']) # <== definimos la ruta
-
-def get(): # <== definimos el metodo que es invocado por la ruta
-  return jsonify({ 'msg': 'hola mundo' })
-
-
-
+# inicializamos el esquema
+smartlog_schema = SmartlogSchema(strict=True)
+smartlogs_schema = SmartlogSchema(many=True, strict=True)
+# corremos el servidor
 if __name__ == '__main__':
-  app.run(debug=True) # <== corremos el servidor
+  app.run(debug=True)
