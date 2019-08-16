1a78267fe2cfc533fe6eb277fcd7f24c9ee8c3c8
Ron Alonzo <alonzo.ronald@gmail.com>
2019-08-16 17:38:58 +0200

    rest api

--- app.py
+++ app.py
@@ -1,69 +1,54 @@
-import os
 from flask import Flask, jsonify, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
+from products import products
 
-# instanciamos la app
-app = Flask(__name__) 
-# creamos la variable de la ruta hacia el archivo sqlite
-basedir = os.path.abspath(os.path.dirname(__file__)) 
-# configuramos la base de datos sqlite
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# Inicializamos la base de datos
-db = SQLAlchemy(app) 
-# Inicializamos Marshmallow
-ma = Marshmallow(app) 
+app = Flask(__name__)
 
-# modelo del producto (que describe como debe ser el objeto)
-class Smartlog(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  date = db.column(db.DateTime)
-  name = db.column(db.String(50))
-  depth = db.column(db.Integer)
-  leave_surface = db.column(db.DateTime)
-  leave_bottom = db.column(db.DateTime)
-  bottom_time = db.column(db.Integer)
+@app.route('/ping')
+def ping():
+  return jsonify({ "message": "Pong!" })
 
-  def __init__(self, date, name, depth, leave_surface, leave_bottom, bottom_time):
-    self.date = date
-    self.name = name
-    self.depth = depth
-    self.leave_surface = leave_surface
-    self.leave_bottom = leave_bottom
-    self.bottom_time = bottom_time
-# se crea el schema (esquema) de el Smartlog
-class SmartlogSchema(ma.Schema):
-  class Meta:
-    fields = ('id', 'date', 'name', 'depth', 'leave_surface', 'leave_bottom', 'bottom_time')
+@app.route('/products')
+def productList():
+  return jsonify({ "message": "Products List", "products": products})
 
-# inicializamos el esquema
-smartlog_schema = SmartlogSchema()
-smartlogs_schema = SmartlogSchema(many=True)
+@app.route('/products/<int:id>')
+def productGet(id):
+  foundList = [product for product in products if product['id'] == id]
+  if (len(foundList) > 0):
+    return jsonify({"products": foundList[0]})
+  return jsonify({ "message": "product not found"})
 
-# crear Smartlog
-@app.route('/smartlogs', methods=['POST'])
-def add_smartlog():
-  date = request.json['date']
-  name = request.json['name']
-  depth = request.json['depth']
-  leave_surface = request.json['leave_surface']
-  leave_bottom = request.json['leave_bottom']
-  bottom_time = request.json['bottom_time']
+@app.route('/products', methods=['POST'])
+def productCreate():
+  newProduct = { 
+	"id": len(request.json['name']), 
+	"name": request.json['name'], 
+	"price": request.json['price'], 
+	"quantity": request.json['quantity']
+  }
+  products.append(newProduct)
+  return jsonify({ "message": "product created", "products": products })
 
-  new_smartlog = Smartlog(date, name, depth, leave_surface, leave_bottom, bottom_time)
-  db.session.add(new_smartlog)
-  db.session.commit()
-  return smartlog_schema.jsonify(new_smartlog)
+@app.route('/products/<int:id>', methods=['PUT'])
+def productEdit(id):
+  foundList = [product for product in products if product['id'] == id]
+  if (len(foundList) > 0):
+    foundList[0]['id'] = request.json['id']
+    foundList[0]['name'] = request.json['name']
+    foundList[0]['quantity'] = request.json['quantity']
+    foundList[0]['price'] = request.json['price']
+    return jsonify({"message": "product updated", "product": products})
 
-# listar todos los smartlogs
-@app.route('/smartlogs', methods=['GET'])
-def get_smartlogs():
-  all_smartlogs = Smartlog.query.all()
-  result = smartlogs_schema.dump(all_smartlogs)
-  print(result)
-  return jsonify(result.data)
+  return jsonify({ "message": "product not found"})
 
-# corremos el servidor
-if __name__ == '__main__':
-  app.run(debug=True)
+@app.route('/products/<int:id>', methods=['DELETE'])
+def productDelete(id):
+  foundList = [product for product in products if product['id'] == id]
+  if (len(foundList) > 0):
+    products.remove(foundList[0])
+    return jsonify({"message": "product deleted", "products": products})
+  return jsonify({ "message": "product not found"})
+
+
+if __name__ == "__main__":
+  app.run(debug=True, port=3002)
---
+++ products.py
@@ -0,0 +1,11 @@
+products = [
+  { "id": 1, "name": "laptop", "price": 800, "quantity": 4 },
+  { "id": 2, "name": "mouse", "price": 30, "quantity": 25 },
+  { "id": 3, "name": "keyboard", "price": 50, "quantity": 2 },
+  { "id": 4, "name": "tv", "price": 600, "quantity": 12 },
+  { "id": 5, "name": "table", "price": 100, "quantity": 33 },
+  { "id": 5, "name": "table", "price": 100, "quantity": 33 },
+  { "id": 5, "name": "table", "price": 100, "quantity": 33 },
+  { "id": 5, "name": "table", "price": 100, "quantity": 33 },
+  { "id": 5, "name": "table", "price": 100, "quantity": 33 }
+]
